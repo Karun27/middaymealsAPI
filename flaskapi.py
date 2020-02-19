871171d39a3e9af2d1b7244a347734cccd356b6d
adityaroyalm <32993154+adityaroyalm@users.noreply.github.com>
2020-02-19 12:23:40 -0500

    update from karunakar mtalk

--- flaskapi.py
+++ flaskapi.py
@@ -7,66 +7,46 @@ This is a temporary script file.
 import pymongo
 from flask import Flask, request,url_for,render_template
 app = Flask(__name__)
-CORS(app)
 import pymongo
 from flask_cors import CORS, cross_origin
-import urllib 
+CORS(app)
+from bson import json_util, ObjectId
+import json
+import urllib
 class aipassflask :
-        def __init__(self):
-            
-            pass
+        def _init_(self):
+
+        pass
         @app.route('/user/login',methods = ['POST','GET'])
-        @cross_origin() 
+        @cross_origin()
         def login():
-            client = pymongo.MongoClient("mongodb+srv://"+urllib.parse.quote("aditya")+':'+urllib.parse.quote("lokam001")+"@cluster0-dikue.mongodb.net/test?retryWrites=true&w=majority")
-            db = client['test']
-            collect=db['logincollection']
-            username = request.form['username']
-            password=request.form['password']
-            doc={'username':username,
-                 'password':password
-                }
-            collect.insert_one(doc)
-            return('success')
+                client = pymongo.MongoClient("mongodb+srv://aditya:lokam001@cluster0-dikue.mongodb.net/test?retryWrites=true&w=majority")
+                db = client['test']
+                collect=db['logincollection']
+                val = request.get_json('email')
+                collect.insert_one(val)
+                page_sanitized = json.loads(json_util.dumps(val))
+                return(page_sanitized)
         @app.route('/user/register',methods = ['POST','GET'])
-        @cross_origin() 
+        @cross_origin()
         def register():
-            client = pymongo.MongoClient("mongodb+srv://"+urllib.parse.quote("aditya")+':'+urllib.parse.quote("lokam001")+"@cluster0-dikue.mongodb.net/test?retryWrites=true&w=majority")
-            db = client['test']
-            collect=db['registercollection']
-            First_Name=request.form['First_Name']
-            Last_Name=request.form['Last_Name']
-            Email=request.form['Email']
-            U=request.form['U']
-            Password=request.form['Password']
-            doc={'First_Name':First_Name,
-                      'Last_Name':Last_Name,
-                      'Email':Email,
-                      'U':U,
-                      'Password':Password
-                }
-            
-            collect.insert_one(doc)
-            return('success')
+                client = pymongo.MongoClient("mongodb+srv://aditya:lokam001@cluster0-dikue.mongodb.net/test?retryWrites=true&w=majority")
+                db = client['test']
+                collect=db['registercollection']
+                val=request.get_json('First_Name')
+                collect.insert_one(val)
+                page_sanitized = json.loads(json_util.dumps(val))
+                return(page_sanitized)
         @app.route('/homepage/home',methods = ['POST','GET'])
-        @cross_origin() 
+        @cross_origin()
         def project():
-            client = pymongo.MongoClient("mongodb+srv://"+urllib.parse.quote("aditya")+':'+urllib.parse.quote("lokam001")+"@cluster0-dikue.mongodb.net/test?retryWrites=true&w=majority")
-            db = client['test']
-            collect=db['registercollection']
-            project =  request.form['Project Name']
-            owner= request.form['Owner']
-            description = request.form['Description']
-            doc={'project':project,
-                 'owner':owner,
-                 'Description':description
-                }
-            collect.insert_one(doc)
-            return('success')
+                client = pymongo.MongoClient("mongodb+srv://aditya:lokam001@cluster0-dikue.mongodb.net/test?retryWrites=true&w=majority")
+                db = client['test']
+                collect=db['homepagecollection']
+                val = request.get_json('project')
+                collect.insert_one(val)
+                page_sanitized = json.loads(json_util.dumps(val))
+                return(page_sanitized)
 
-if __name__ == '__main__':
-    app.run()
-    
-
-                
-        
+if _name_ == '__main__':
+app.run()
