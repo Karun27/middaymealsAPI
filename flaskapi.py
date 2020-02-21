f53142ee7b8a2a30493c534678055dc5ad427f66
adityaroyalm <32993154+adityaroyalm@users.noreply.github.com>
2020-02-21 10:52:19 -0500

    updated urls,added feda

--- flaskapi.py
+++ flaskapi.py
@@ -28,7 +28,6 @@ class aipassflask :
             val = request.get_json('email')
             collect.insert_one(val)
             page_sanitized = json.loads(json_util.dumps(val))
-            
             return(page_sanitized)
         @app.route('/user/register',methods = ['POST','GET'])
         @cross_origin()
@@ -50,7 +49,7 @@ class aipassflask :
             collect.insert_one(val)
             page_sanitized = json.loads(json_util.dumps(val))
             return(page_sanitized)
-        @app.route('/connections/new',methods = ['POST','GET'])
+        @app.route('/connectionsnew',methods = ['POST','GET'])
         @cross_origin()
         def connections():
             client = pymongo.MongoClient("mongodb+srv://aditya:lokam001@cluster0-dikue.mongodb.net/test?retryWrites=true&w=majority")
@@ -63,18 +62,28 @@ class aipassflask :
             a= sc.HDFSConnection()
             b=sc.KafkaConnection()
             return({'HDFS':a,'Kafka':b,'Ubuntu':c})
-        @app.route('/datasource/new',methods = ['POST','GET'])
+        @app.route('/datasourcenew',methods = ['POST','GET'])
         @cross_origin()
         def datasource():
                         client = pymongo.MongoClient("mongodb+srv://aditya:lokam001@cluster0-dikue.mongodb.net/test?retryWrites=true&w=majority")
                         db = client['test']
-                        collect=db['datasourceconnection']
+                        collect=db['datasourcecollection']
                         val = request.get_json('selectDataType')
                         collect.insert_one(val)
                         page_sanitized = json.loads(json_util.dumps(val))
                         return(page_sanitized)
         
-            
+        @app.route('/new',methods = ['POST','GET'])
+        @cross_origin()
+        def featureeng():
+                        client = pymongo.MongoClient("mongodb+srv://aditya:lokam001@cluster0-dikue.mongodb.net/test?retryWrites=true&w=majority")
+                        db = client['test']
+                        collect=db['fedacollection']
+                        val = request.get_json('options1.name')
+                        collect.insert_one(val)
+                        page_sanitized = json.loads(json_util.dumps(val))
+                        return(page_sanitized)
+                        
 if __name__ == '__main__':
     app.run()
 
