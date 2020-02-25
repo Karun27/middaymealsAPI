97132c78d2d4adf95bfc2e7730ec3f8ae75a9fad
adityaroyalm <32993154+adityaroyalm@users.noreply.github.com>
2020-02-25 12:35:06 -0500

    updated feda data structure change

--- flaskapi.py
+++ flaskapi.py
@@ -15,9 +15,9 @@ CORS(app)
 from bson import json_util, ObjectId
 import json
 import urllib
-import KafkaTopic
-import hdfs_connection
-import fedanew
+#import KafkaTopic
+#import hdfs_connection
+import featurenames
 class aipassflask :
         def __init__(self):
 
@@ -121,12 +121,33 @@ class aipassflask :
         @app.route('/feda',methods = ['POST','GET'])
         @cross_origin()
         def feda():
-            val=request.get_json('fengg')
-            page_sanitized= json.loads(json_util.dumps(val))
-            fedanew.fedaclass.main(val)
-            return page_sanitized
+            client = pymongo.MongoClient("mongodb+srv://aditya:lokam001@cluster0-dikue.mongodb.net/test?retryWrites=true&w=majority")
+
+            db = client['test']
+            a=[]
+            cursor = db.connectionscollection.find({})
+            for document in cursor:
+                a.append(document)
+                #val=session['myvar']
+            a=sorted(a,key= lambda x:x['_id'])[-1]
+            key=[]
+            key_value=[]
+            for i in range(len(a['val'])):
+                print(a['val'])
+                key.append(a['val'][i]['Feature_Engeneering_option'])
+                value=[]
+                for j in range(len(a['val'][i]['Features'])):
+                    value.append(a['val'][i]['Features'][j]['name'])
+                key_value.append(value)
+            val={}
+            for i,j in zip(key,key_value):
+                val[i]=j
+            valnew=request.json
+            page_sanitized= json.loads(json_util.dumps(valnew))
+            featurenames.final(page_sanitized,val.get('host'),val.get('user'),val.get('port'),val.get('path'),val.get('file'))
+            return page_sanitizedsa
             
-            
+#[{"Feature_Engeneering_option":"Bucketing","Features":[{"name":"acds"},{"name":"asd"}]},{"Feature_Engeneering_option":"Handle missing data","Features":[{"name":"dsfsc"},{"name":"asd"}]},{"Feature_Engeneering_option":"Drop duplicate data","Features":[{"name":"asd"}]}]           
                         
 if __name__ == '__main__':
         app.run()
