cd140b7c9620dadc244c67d3665db6a0efc6d1a4
adityaroyalm <32993154+adityaroyalm@users.noreply.github.com>
2020-02-26 11:09:26 -0500

    added recommend api

--- flaskapi.py
+++ flaskapi.py
@@ -76,35 +76,35 @@ class aipassflask :
         @cross_origin()
         def datasource():
                         #connections()
-                        client = pymongo.MongoClient("mongodb+srv://aditya:lokam001@cluster0-dikue.mongodb.net/test?retryWrites=true&w=majority")
+            client = pymongo.MongoClient("mongodb+srv://aditya:lokam001@cluster0-dikue.mongodb.net/test?retryWrites=true&w=majority")
 
-                        db = client['test']
-                        a=[]
-                        cursor = db.connectionscollection.find({})
-                        for document in cursor:
-                            a.append(document)
-                        #val=session['myvar']
-                        val=sorted(a,key= lambda x:x['_id'])[-1]
-                        val_port = request.get_json('topic_Name')
-                        #session['my_var2'] = val_port
-                        
-                        page_sanitized= json.loads(json_util.dumps(val_port))
-                        sc=KafkaTopic.KafkaTopic(val,page_sanitized)
-                        sc.topic()
-                        sc.producer()
-                        sc.consumer()  
-                        host=val.get('host')
-                        user=val.get('user')
-                        port=val.get('port')
-                        path=page_sanitized.get('path')
-                        file=page_sanitized.get('file')
-                        cols=fedanew.dataconn(host,user,port,path,file)[1]
-                        val_port['cols']=cols
-                        db = client['test']
-                        collect=db['datasourcecollection']
-                        collect.insert_one(val_port)
-                        
-                        return(cols)
+            db = client['test']
+            a=[]
+            cursor = db.connectionscollection.find({})
+            for document in cursor:
+                a.append(document)
+            #val=session['myvar']
+            val=sorted(a,key= lambda x:x['_id'])[-1]
+            val_port = request.get_json('topic_Name')
+            #session['my_var2'] = val_port
+            
+            page_sanitized= json.loads(json_util.dumps(val_port))
+            sc=KafkaTopic.KafkaTopic(val,page_sanitized)
+            sc.topic()
+            sc.producer()
+            sc.consumer()  
+            host=val.get('host')
+            user=val.get('user')
+            port=val.get('port')
+            path=page_sanitized.get('path')
+            file=page_sanitized.get('file')
+            cols=fedanew.dataconn(host,user,port,path,file)[1]
+            val_port['cols']=cols
+            db = client['test']
+            collect=db['datasourcecollection']
+            collect.insert_one(val_port)
+            
+            return(cols)
         @app.route('/features',methods = ['POST','GET'])
         @cross_origin()
         def features():
@@ -145,13 +145,38 @@ class aipassflask :
             valnew=request.json
             page_sanitized= json.loads(json_util.dumps(valnew))
             featurenames.final(page_sanitized,val.get('host'),val.get('user'),val.get('port'),val.get('path'),val.get('file'))
-            return page_sanitizedsa
+            return page_sanitized
+        @app.route('/recommend',methods = ['POST','GET'])
+        @cross_origin()
+        def recommend():
+            client = pymongo.MongoClient("mongodb+srv://aditya:lokam001@cluster0-dikue.mongodb.net/test?retryWrites=true&w=majority")
+
+            db = client['test']
+            a=[]
+            cursor = db.connectionscollection.find({})
+            for document in cursor:
+                a.append(document)
+                #val=session['myvar']
+            a=sorted(a,key= lambda x:x['_id'])[-1]
+            key=[]
+            key_value=[]
+            for i in range(len(a['val'])):
+                print(a['val'])
+                key.append(a['val'][i]['Feature_Engeneering_option'])
+                value=[]
+                for j in range(len(a['val'][i]['Features'])):
+                    value.append(a['val'][i]['Features'][j]['name'])
+                key_value.append(value)
+            val={}
+            for i,j in zip(key,key_value):
+                val[i]=j
+            valnew=request.json
+            valnew=valnew['cols']
+            page_sanitized= json.loads(json_util.dumps(valnew))
+            featurenames.final(page_sanitized,val.get('host'),val.get('user'),val.get('port'),val.get('path'),val.get('file'))
+            return page_sanitized
             
 #[{"Feature_Engeneering_option":"Bucketing","Features":[{"name":"acds"},{"name":"asd"}]},{"Feature_Engeneering_option":"Handle missing data","Features":[{"name":"dsfsc"},{"name":"asd"}]},{"Feature_Engeneering_option":"Drop duplicate data","Features":[{"name":"asd"}]}]           
                         
 if __name__ == '__main__':
         app.run()
-
-
-
-
