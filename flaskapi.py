587d9f3f50bfe4602fb13c6d029bcfde279b0722
adityaroyalm <32993154+adityaroyalm@users.noreply.github.com>
2020-03-02 13:40:06 -0500

    added 2 more api's

--- flaskapi.py
+++ flaskapi.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 20 15:21:55 2020
-
-@author: adityaroyal
-"""
-
 import pymongo
 from flask import Flask, request,url_for,render_template,session,redirect
 app = Flask(__name__)
@@ -88,13 +81,14 @@ class aipassflask :
             sc=KafkaTopic.KafkaTopic(val,page_sanitized)
             sc.topic()
             sc.producer()
-            sc.consumer()  
+            sc.consumer()
             host=val.get('host')
             user=val.get('user')
             port=val.get('port')
-            path=page_sanitized.get('source_Path')
-            file=page_sanitized.get('source_File')
-            cols=featurenames.dataconn(host,user,port,path,file)[1]
+            path=page_sanitized.get('data_Store_Path')
+            folder=page_sanitized.get('data_Store_Folder')
+            file=page_sanitized.get('data_Store_Filename')
+            cols=featurenames.dataconn(host,user,port,path,folder,file)[1].tolist()
             val_port['cols']=cols
             db = client['test']
             collect=db['datasourcecollection']
@@ -106,6 +100,7 @@ class aipassflask :
         def features():
             client = pymongo.MongoClient("mongodb+srv://aditya:lokam001@cluster0-dikue.mongodb.net/test?retryWrites=true&w=majority")
             db = client['test']
+            collect=db['datasourcecollection']
             cursor = db.datasourcecollection.find({})
             a=[]
             for document in cursor:
@@ -114,6 +109,18 @@ class aipassflask :
             val=sorted(a,key= lambda x:x['_id'])[-1]
             cols=val['cols']
             return({'cols':cols})
+        @app.route('/submit',methods = ['POST','GET'])
+        @cross_origin()
+        def submit():
+           client = pymongo.MongoClient("mongodb+srv://aditya:lokam001@cluster0-dikue.mongodb.net/test?retryWrites=true&w=majority")
+           db = client['test']
+           collect=db['columnscollection']
+           val_port=request.json
+           value=[k['name'] for k in val_port]
+           di={}
+           di['newcols']=value
+           collect.insert_one(di)
+           return ({'cols':value})
         @app.route('/feda',methods = ['POST','GET'])
         @cross_origin()
         def feda():
@@ -146,12 +153,18 @@ class aipassflask :
                 a.append(document)
             #val=session['myvar']
             vald=sorted(a,key= lambda x:x['_id'])[-1]
-           
+            a=[]
+            featurepath=vald.get('featurepath')
+            collect=db['dmpfolderscollection']
+            di={}
+            di['featurepath']=featurepath
+            collect.insert_one(di)
             featurenames.final(val,valc.get('host'),valc.get('user'),valc.get('port'),vald.get('path'),vald.get('file'),vald.get('folder'),vald.get('featurepath'),vald.get('featurefile'),vald.get('featurefolder'))
             return page_sanitized
         @app.route('/recommend',methods = ['POST','GET'])
         @cross_origin()
         def recommend():
+            print('success5')
             client = pymongo.MongoClient("mongodb+srv://aditya:lokam001@cluster0-dikue.mongodb.net/test?retryWrites=true&w=majority")
 
             db = client['test']
@@ -167,15 +180,59 @@ class aipassflask :
                 a.append(document)
             #val=session['myvar']
             vald=sorted(a,key= lambda x:x['_id'])[-1]
-           
-            featurenames.final(vald.get('cols'),valc.get('host'),valc.get('user'),valc.get('port'),vald.get('path'),vald.get('file'),vald.get('folder'),vald.get('featurepath'),vald.get('featurefile'),vald.get('featurefolder'))    
-            return page_sanitized
-            
+            a=[]
+            cursor = db.columnscollection.find({})
+            for document in cursor:
+                a.append(document)
+            valnc=sorted(a,key= lambda x:x['_id'])[-1]
+
+            print('success6')
+            print(vald.get('newcols'),valc.get('host'),valc.get('port'),valc.get('user'),vald.get('data_Store_Path'),vald.get('data_Store_Folder'),vald.get('data_Store_Filename'),vald.get('feature_Store_Path'),vald.get('feature_Store_Folder'),vald.get('feature_Store_Filename'))
+            df=feda_auto.main(valnc.get('cols').tolist(),valc.get('host'),valc.get('port'),valc.get('user'),vald.get('data_Store_Path'),vald.get('data_Store_Folder'),vald.get('data_Store_Filename'),vald.get('feature_Store_Path'),vald.get('feature_Store_Folder'),vald.get('feature_Store_Filename'))    
+            return (df)
+            #self, column_list, host, webhdfs_default_port, user_ubuntu, datastore_path, datastore_foldername, datastore_filename, featurestore_path, featurestore_foldername, featurestore_filename
 #[{"Feature_Engeneering_option":"Bucketing","Features":[{"name":"acds"},{"name":"asd"}]},{"Feature_Engeneering_option":"Handle missing data","Features":[{"name":"dsfsc"},{"name":"asd"}]},{"Feature_Engeneering_option":"Drop duplicate data","Features":[{"name":"asd"}]}]           
-                        
-if __name__ == '__main__':
-        app.run()
-
-
-
-
+            @app.route('/dmpfolders',methods = ['POST','GET'])
+            @cross_origin()
+            def dmpfolders():
+               client = pymongo.MongoClient("mongodb+srv://aditya:lokam001@cluster0-dikue.mongodb.net/test?retryWrites=true&w=majority")
+               db = client['test']
+               a=[]
+               cursor = db.dmpfolderscollection.find({})
+               for document in cursor:
+                   a.append(document)
+               valnc=sorted(a,key= lambda x:x['_id'])[-1]
+               featurepath=valnc.get('featurepath')
+               do=glob(directory)
+               childdirectory=[x.split('\\')[-1] for x in do]
+               file=[]
+               di={}
+               for x in childdirectory:
+                   do=directory[:-1]+x+'\*'
+                   do=glob(do)
+                   di[str(x)]=[x.split('\\')[-1] for x in do]
+               return (di)
+           @app.route("/dmpreaddata",methods=['POST'])
+           @cross_origin()
+           def dmpreaddata():
+                input1=json.loads(request.data)
+                folder=input1.get('folder')
+                file=input1.get('file')  
+                client = pymongo.MongoClient("mongodb+srv://aditya:lokam001@cluster0-dikue.mongodb.net/test?retryWrites=true&w=majority")
+                db = client['test']
+                a=[]
+                cursor = db.datasourcecollection.find({})
+                for document in cursor:
+                    a.append(document)
+                #val=session['myvar']
+                vald=sorted(a,key= lambda x:x['_id'])[-1]
+                path=vald.get('featurepath')
+                db = client['test']
+                a=[]
+                cursor = db.connectionscollection.find({})
+                for document in cursor:
+                    a.append(document)
+                #val=session['myvar']
+                valc=sorted(a,key= lambda x:x['_id'])[-1]
+                cols=featurenames.dataconn(valc.get('host'),valc.get('port'),valc.get('user'),path,folder,file)[1].tolist()   
+                return jsonify(col)
